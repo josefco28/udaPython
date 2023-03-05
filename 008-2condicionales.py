1b4f47ee15cc0bc20089350d79b0bbd8be285376
jose <jose.mtz.mdz3@gmail.com>
2023-03-04 20:44:38 -0600

    conicionales 2 primer commit

---
+++ 008-2condicionales.py
@@ -0,0 +1,9 @@
+ocupacion = 'empleado'
+if ocupacion == 'estudiante':
+    print('tienes 50 % de descuento')
+elif ocupacion == 'jubilado':
+    print('tienes 75 % de descuento')
+elif ocupacion == 'desempleado':
+    print('tienes un 10 % de descuento')
+else:
+    print('tienes que pagar el 100 %')
