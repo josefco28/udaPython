5c78954a52318624a29e9f4f15ae42b8a309a597
jose <jose.mtz.mdz3@gmail.com>
2023-03-04 19:22:22 -0600

    condicionales

---
+++ 008.py
@@ -0,0 +1,11 @@
+#condicionales.py  
+usuario_autenticado = False
+usuario_admin = False
+if usuario_autenticado:
+    if usuario_admin:
+       print('acceso total')
+    else:
+        print("acceso al sitema")
+else:
+        print("debes iniciar secion") 
+   
